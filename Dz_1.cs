79fdad99aaaa682a0403fcd465e808ac74f4fc45
superproger228 <61800512+superproger228@users.noreply.github.com>
2020-11-26 00:06:18 +0300

    Add files via upload

---
+++ Dz_1.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rePoli;
+            string poli = Console.ReadLine();
+            poli = poli.Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "").Replace("?", "").Replace(";", "").Replace(":", "").Replace("-", "");
+            poli = poli.ToLower();
+
+            char[] sRev = poli.ToCharArray();
+            Array.Reverse(sRev);
+            rePoli = new string(sRev);
+
+            Console.WriteLine(
+            rePoli == poli
+            ? "Строка - палиндром"
+            : "Ну строка, как строка (не палиндром)"
+            );
+        }
+    }
+}
