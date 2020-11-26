ce0fde7756d710195885c5661ca9007def228aee
superproger228 <61800512+superproger228@users.noreply.github.com>
2020-11-26 22:15:16 +0300

    Add files via upload

---
+++ Dz_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string set = Console.ReadLine();
+            string poli = "";
+            foreach (char c in set)
+            {
+                if (char.IsLetter(c) || char.IsDigit(c))
+                {
+                    poli += char.ToLower(c);
+                }
+            }
+            bool isPalindrom = true;
+            for (int i = 0; i < poli.Length / 2; i++)
+            {
+                if (poli[i] != poli[poli.Length - i - 1])
+                {
+                    isPalindrom = false;
+                }
+            }
+
+            Console.Write($"\"{set}\" is ");
+            Console.WriteLine(isPalindrom
+            ? "палиндром"
+            : "не палиндром");
+        }
+    }
+}
